544c2ed6261fa31e6845f603942b46408f66e985
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-05-13 11:35:05 +0300

    Add classes

---
+++ domain/src/main/java/products/Product.java
@@ -0,0 +1,14 @@
+package products;
+
+public class Product {
+
+    private final String name;
+    private final int rate;
+    private final double price;
+
+    public Product(String name, int rate, double price) {
+        this.name = name;
+        this.rate = rate;
+        this.price = price;
+    }
+}
