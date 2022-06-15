78482ddd66a68c42709709deea324a5655f94aea
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-06-15 10:32:55 +0300

    Use reflection to get categories

--- store/src/main/java/Store.java
+++ store/src/main/java/Store.java
@@ -1,3 +1,41 @@
+import by.issoft.categories.Category;
+import org.reflections.Reflections;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+
 public class Store {
 
+    public static Map<Category, Integer> createCategoryMap() {
+        Map<Category, Integer> categoryToPut = new HashMap<>();
+
+        Reflections reflections = new Reflections("com.issoft.domain.categories");
+
+        Set<Class<? extends Category>> subTypes = reflections.getSubTypesOf(Category.class);
+
+        for (Class<? extends Category> type : subTypes) {
+            try {
+                Random random = new Random();
+                categoryToPut.put(type.getConstructor().newInstance(), random.nextInt(10));
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return categoryToPut;
+    }
 }
+
+
+
+
+
+
