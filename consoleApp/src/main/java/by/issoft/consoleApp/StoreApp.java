b29638f3bd8794e4ab8d2792d4d8ae2ed88e2e6a
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-08-04 01:13:02 +0300

    add possibility to execute commands several times

--- consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
@@ -5,13 +5,11 @@ import by.issoft.store.helpers.SortHelper;
 import by.issoft.store.helpers.StoreHelper;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class StoreApp {
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException {
+    public static void main(String[] args) throws ParserConfigurationException {
 
         Store onlineStore = new Store();
         StoreHelper storeHelper = new StoreHelper(onlineStore);
@@ -19,21 +17,22 @@ public class StoreApp {
         storeHelper.fillStore();
         //onlineStore.printStore();
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String command = bufferedReader.readLine();
-
-        switch (command) {
-            case "sort":
-                sortHelper.sortByXML(onlineStore);
-                break;
-            case "top":
-                sortHelper.topFiveExpensiveProducts(onlineStore);
-                break;
-            case "quit":
-                System.exit(0);
-            default:
-                System.out.println("Command is not supported.");
+        Scanner sc = new Scanner(System.in);
+        String command;
+        while(sc.hasNext()){
+            command = sc.nextLine();
+            switch (command) {
+                case "sort":
+                    sortHelper.sortByXML(onlineStore);
+                    break;
+                case "top":
+                    sortHelper.topFiveExpensiveProducts(onlineStore);
+                    break;
+                case "quit":
+                    System.exit(0);
+                default:
+                    System.out.println("Command is not supported.");
+                }
+            }
         }
     }
-}
