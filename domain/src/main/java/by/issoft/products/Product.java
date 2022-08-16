6b8a4116aa4aff74d06a2800d02bcc73bd147a55
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-08-17 00:17:47 +0300

    add Singleton pattern

--- domain/src/main/java/by/issoft/products/Product.java
+++ domain/src/main/java/by/issoft/products/Product.java
@@ -2,16 +2,41 @@ package by.issoft.products;
 
 public class Product {
 
-    private final String name;
-    private final double rate;
-    private final double price;
+    private String name;
+    private double rate;
+    private double price;
 
-    public Product(String name, double price, Double rate) {
-        this.name = name;
-        this.price = price;
-        this.rate = rate;
+    public static ProductBuilder newProductBuilder() {
+        return new Product().new ProductBuilder();
     }
 
+    public class ProductBuilder {
+        private String name;
+        private double rate;
+        private double price;
+
+        private ProductBuilder() {
+        }
+
+        public ProductBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public ProductBuilder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+        public ProductBuilder setRate(double rate) {
+            this.rate = rate;
+            return this;
+        }
+        public Product build() {
+            Product.this.name = this.name;
+            Product.this.price = this.price;
+            Product.this.rate = this.rate;
+            return Product.this;
+        }
+    }
     public String getName() {
         return name;
     }
