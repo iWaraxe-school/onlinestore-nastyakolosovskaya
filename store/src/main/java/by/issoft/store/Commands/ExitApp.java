2446f1c781f5314010f86b19f648005a67db7dfb
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-09-21 17:13:31 +0300

    Add exit command

--- store/src/main/java/by/issoft/store/Commands/ExitApp.java
+++ store/src/main/java/by/issoft/store/Commands/ExitApp.java
@@ -1,14 +1,9 @@
 package by.issoft.store.Commands;
 
 public class ExitApp implements StoreCommand {
-
-    public void StopApp(){
-        System.out.println("Hope to see you again!");
-        System.exit(0);
-    }
-
     @Override
     public void execute() {
-        StopApp();
+        System.out.println("Hope to see you again!");
+        System.exit(0);
     }
 }
