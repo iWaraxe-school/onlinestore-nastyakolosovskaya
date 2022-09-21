f987e956a1d7056b43bce10be7a3bd4316377a87
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-09-21 17:14:15 +0300

    Fix OrderProduct command Remove join()

--- store/src/main/java/by/issoft/store/Commands/OrderProduct.java
+++ store/src/main/java/by/issoft/store/Commands/OrderProduct.java
@@ -5,16 +5,11 @@ import by.issoft.store.helpers.CreateOrder;
 
 public class OrderProduct implements StoreCommand{
 
-     private final CreateOrder createOrder = new CreateOrder();
 
     @Override
     public void execute()  {
 
-           createOrder.start();
-        try {
-            createOrder.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+        CreateOrder createOrder = new CreateOrder();
+        createOrder.start();
+          }
 }
