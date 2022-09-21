3f5511ed69b8b6af8bc827d8165ec34d43e747d5
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2022-09-21 17:11:13 +0300

    Remove while loop Add counter for threads

--- store/src/main/java/by/issoft/store/helpers/CreateOrder.java
+++ store/src/main/java/by/issoft/store/helpers/CreateOrder.java
@@ -8,27 +8,28 @@ import java.util.concurrent.TimeUnit;
 
 public class CreateOrder extends Thread {
     private final Store store = Store.getInstance();
+    private  static  int counter = 0;
 
     @Override
     public void run() {
-
-        while (!Thread.currentThread().isInterrupted()) {
-            Thread.currentThread().setName("orderThread");
+            counter++;
+            Thread.currentThread().setName("orderThread #" + counter);
             System.out.println("Thread name: " + Thread.currentThread().getName());
             Product purchasedProduct = store.getAllProducts().get(new Random().nextInt(10));
 
             System.out.println("Ordered product: " + purchasedProduct);
             store.getPurchasedProductList().add(purchasedProduct);
             System.out.println(store.getPurchasedProductList());
+
             try {
                 TimeUnit.SECONDS.sleep(new Random().nextInt(30));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-
             }
-        }
+        System.out.println(Thread.currentThread().getName());
     }
 }
+
 
 
 
